a77ee661386b3d3c8a02229d70c412f942210794
barinova-alevel <you@example.com>
2021-09-19 10:49:57 +0300

    Add project files.

---
+++ Module3HW4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module3HW4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
